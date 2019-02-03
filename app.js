3c7e45b0edb0a0906e15b0cbdefba3a0ef6de2d6
TheSweerX <thesweer@gmail.com>
2019-02-03 21:52:38 +0100

    make working node server for the desk

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const http = require('http');
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 
 var app = express();
+app.set('view engine', 'ejs');
 
-app.set('view engine', 'jade');
-app.engine('jade', require('jade').__express); 
- 
-app.get('/deskcontrol', function(req, res) {
-    res.sendFile('./deskcontrol.html', {root: __dirname })
-});
+app.use(express.static(path.join(__dirname, 'public')));
+console.log(path.join(__dirname, 'public'));
 
-var port = process.env.PORT || 3000;
-var server = app.listen(port);
-console.log('Express app started');
+app.get('/', function(req, res){
+    res.render('index',{status:"Press Button"});
+})
+
+app.post('/desk/up', function(req, res){
+    console.log('Lifting the table');
+    return res.render('index', {status: "Desk went up"});
+})
+
+app.post('/desk/down', function(req, res){
+    console.log('Lowering the table');
+    return res.render('index', {status: "Desk went down"});
+})
+
+app.listen(3000, function () {
+    console.log('Server started on Port: 3000!')
+})
