93b3d3a3cd5baa068f8abc02879b53210c85a2d2
TheSweerX <thesweer@gmail.com>
2020-07-13 21:41:59 +0200

    green, and numbers

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const http = require('http');
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const cmd = require('node-cmd');
 
 var app = express();
 app.set('view engine', 'ejs');
@@ -22,18 +21,6 @@ app.post('/desk/up', function(req, res){
 app.post('/desk/down', function(req, res){
     console.log('Lowering the table');
     return res.render('index', {status: "Desk went down"});
-})
-
-app.post('/windows/calc', function(req, res){
-    console.log('starting calculator');    
-    cmd.run("calc");
-    return res.render('index', {status: "Calculator started"});
-})
-
-app.post('/windows/notepad', function(req, res){
-    console.log('starting notepad');    
-    cmd.run("notepad");
-    return res.render('index', {status: "Notepad started"});
 })
 
 app.listen(3000, function () {
